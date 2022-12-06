5fa6a7da74c3ac2819d40b288d8b0ba87bee4b61
Sergio Fidelis <sergiofidelis31@hotmail.com>
2022-12-06 13:28:46 -0300

    Add h2 dependency

---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (email, first_name, last_name, level) VALUES('jane@doe.com', 'jane', 'doe', 1);
+INSERT INTO users (email, first_name, last_name, level) VALUES('john@doe.com', 'john', 'doe', 2);
